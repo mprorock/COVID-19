7cd27da9f027df985f03255132f29a1e604ebfca
Orie Steele <orie@transmute.industries>
2020-03-19 21:30:14 -0500

    updates

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 
-git remote add upstream https://github.com/CSSEGISandData/COVID-19.git
+git remote add upstream https://github.com/CSSEGISandData/COVID-19.git;
 
-git add .
-git commit -a -m "updates"; git fetch upstream ; git merge upstream/master
+git add .;
+git commit -a -m "updates"; 
+git fetch upstream ; 
+git merge upstream/master;
